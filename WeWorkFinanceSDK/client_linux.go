1ac29f4324cf9f2f80291a48f16f3a4a42d411e0
wener <wenermail@gmail.com>
2023-05-01 01:04:26 +0800

    mv Unmarshal to all platform

--- WeWorkFinanceSDK/client_linux.go
+++ WeWorkFinanceSDK/client_linux.go
@@ -356,29 +356,6 @@ func DecryptData(privateKey *rsa.PrivateKey, encryptRandomKey string, encryptMsg
 	return buf, err
 }
 
-func Unmarshal(data []byte) (msg Message, err error) {
-	var getType struct {
-		Type   string `json:"msgtype,omitempty"`
-		Action string `json:"action,omitempty"`
-	}
-	err = json.Unmarshal(data, &getType)
-	if err != nil {
-		return msg, err
-	}
-	msg = MessageOfType(getType.Action, getType.Type)
-	msg.SetRaw(data)
-	err = json.Unmarshal(data, msg)
-	return msg, err
-}
-
-// UnmarshalToBase can avoid json error
-func UnmarshalToBase(data []byte) (msg *BaseMessage, err error) {
-	msg = &BaseMessage{}
-	msg.SetRaw(data)
-	err = json.Unmarshal(data, &msg)
-	return msg, err
-}
-
 // type counterWriter struct {
 // 	Count int
 // }
